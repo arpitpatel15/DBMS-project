49024a10480fa371b72786a9b9a97879426c54ea
arpitpatel15 <codewithme442@gmail.com>
2024-08-01 16:18:07 +0530

    Add files via upload

---
+++ Petrol Pump Management System/Project/Create_database.py
@@ -0,0 +1,14 @@
+import mysql.connector
+
+mydb = mysql.connector.connect(
+   host="localhost",
+   user="root",
+   password="",
+   database = "Petrolpump_Management"
+)
+
+c = mydb.cursor()
+mydb.commit()
+mydb.close()
+
+
